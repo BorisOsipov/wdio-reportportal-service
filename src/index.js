3a5968484ebeab6a5a0d7cb35942f1dc7a0c03e0
Boris Osipov <osipov.boris@gmail.com>
2021-03-17 16:25:20 +0300

    Simplify options

--- src/index.js
+++ src/index.js
@@ -25,12 +25,8 @@ class RpService {
 
     const {id} = await promise;
     process.env.RP_LAUNCH_ID = id;
-    if (this.options && this.options.writeLaunchIdToFile) {
-      if (this.options.writeLaunchIdToFile.toString() !== "true") {
-        fs.writeFileSync(this.options.writeLaunchIdToFile, id);
-      } else {
-        fs.writeFileSync('./RP_LAUNCH_ID', id);
-      }
+    if (this.options && this.options.launchIdFilePath) {
+        fs.writeFileSync(this.options.launchIdFilePath, id);
     }
     return promise;
   }
